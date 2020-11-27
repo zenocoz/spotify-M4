088c0c63d4760166f48b22a431fa34f299c7d51b
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-27 16:07:14 +0100

    made Album function into class

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,41 +1,48 @@
 import React from "react"
 import {Link} from "react-router-dom"
 
-export default function Album(props) {
-  return (
-    <section className="single-album">
-      <div className="row">
-        <div className="col-12 col-lg-4 px-4">
-          <div className="card">
-            <img className="card-img-top" src="" alt="Card image cap" />
-            <div className="card-body d-flex flex-column align-items-center">
-              <h5 className=" card-title"></h5>
+class Album extends React.Component {
+  state = {}
 
-              <h6>Queen</h6>
+  componentDidMount = () => {}
+  render() {
+    return (
+      <section className="single-album">
+        <div className="row">
+          <div className="col-12 col-lg-4 px-4">
+            <div className="card">
+              <img className="card-img-top" src="" alt="Card image cap" />
+              <div className="card-body d-flex flex-column align-items-center">
+                <h5 className=" card-title"></h5>
 
-              <div className="d-flex flex-column buttons-wrapper align-items-center">
-                <button
-                  type="button"
-                  className="btn-login btn-green d-inline"
-                  style={{marginRight: 0}}
-                >
-                  Play
-                </button>
-                <h6>1974 - 16 songs</h6>
-                <div className="icons-wrapper">
-                  <i className="fa fa-heart" aria-hidden="true"></i>
-                  <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
+                <h6>Queen</h6>
+
+                <div className="d-flex flex-column buttons-wrapper align-items-center">
+                  <button
+                    type="button"
+                    className="btn-login btn-green d-inline"
+                    style={{marginRight: 0}}
+                  >
+                    Play
+                  </button>
+                  <h6>1974 - 16 songs</h6>
+                  <div className="icons-wrapper">
+                    <i className="fa fa-heart" aria-hidden="true"></i>
+                    <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="col-12 col-lg-8">
+            <table className="table table-borderless table-dark table-album">
+              <tbody className="song-list"></tbody>
+            </table>
+          </div>
         </div>
-        <div className="col-12 col-lg-8">
-          <table className="table table-borderless table-dark table-album">
-            <tbody className="song-list"></tbody>
-          </table>
-        </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
+
+export default Album
