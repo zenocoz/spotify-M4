3704483ec0c2692bdbfffef690198f1406549a0f
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-27 11:08:59 +0100

    converted NavBar to jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Route} from "react-router-dom"
 import Home from "./components/Home"
 import Artist from "./components/Artist"
 import Album from "./components/Album"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Route path="/" exact component={Home} />
         <Route path="/artist" exact component={Artist} />
         <Route path="/album" exact component={Album} />
