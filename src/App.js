5642f77e0684afbddfff22bd6a33343442de0ba9
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-27 10:02:06 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import Home from "./components/Home"
+import Artist from "./components/Artist"
+import Album from "./components/Album"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/artist" exact component={Artist} />
+        <Route path="/album" exact component={Album} />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Album.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Album() {
+  return <div>Album Page</div>
+}
